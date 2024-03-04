2aaa5db0bf4c4cf488ced3386484d32ee0482fe7
shreyansh232 <shreyansh487@gmail.com>
2024-03-04 18:56:20 +0530

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Shreyansh Pandey on 04/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.blue)
+                .edgesIgnoringSafeArea(.all)
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
